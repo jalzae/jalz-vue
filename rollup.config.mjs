1a3214f742944a319928eedc80002c139ec24e23
jalzae <jalzae@gmail.com>
2024-08-23 21:55:05 +0700

    first

---
+++ rollup.config.mjs
@@ -0,0 +1,20 @@
+import vue from 'rollup-plugin-vue';
+import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'esm',
+  },
+  plugins: [
+    vue(),
+    postcss({
+      extract: true,
+    }),
+    resolve(),
+    commonjs(),
+  ]
+};
---
+++ src/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-component p-4 bg-blue-500 text-white">
+    <p>{{title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+  },
+  name: "MyComponent",
+};
+</script>
+
+<style scoped>
+@import "tailwindcss/tailwind.css";
+</style>
